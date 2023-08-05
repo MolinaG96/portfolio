0cb999906cc1ebfd510e1be9e2cb8eeaab5941c4
Gonzalo <molinagomezgonzalo@gmail.com>
2023-08-05 03:14:21 -0300

    feat: navbar hide and pull

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,20 @@
-import React from "react";
-import "../styles/navbar.css"; // Crearemos estilos para la barra de navegación en este archivo
+import React, { useState } from "react";
+import "../styles/navbar.css";
 import mano from "../assets/mano.png";
 import soga from "../assets/soga-nada-sinF.png";
 import madera from "../assets/boton-madera.png";
 
 const Navbar = () => {
+  const [isNavbarHidden, setIsNavbarHidden] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsNavbarHidden(!isNavbarHidden);
+    console.log("soy la mano", isNavbarHidden);
+  };
   return (
-    <div className="navbar-container">
-      {/* Mano */}
-      <img src={mano} alt="Mano" className="mano" />
-      {/* Soga superior */}
+    <div className={`navbar-container ${isNavbarHidden ? "hidden" : ""}`}>
+      <img src={mano} alt="Mano" className="mano" onClick={toggleNavbar} />
       <img src={soga} alt="Soga" className="soga-top" />
-      {/* Maderas */}
       <img
         src={madera}
         alt="Madera"
@@ -42,87 +45,9 @@ const Navbar = () => {
         className="madera"
         style={{ left: "900px" }}
       />
-      {/* Soga inferior */}
       <img src={soga} alt="Soga" className="soga-bottom" />
     </div>
   );
 };
 
 export default Navbar;
-
-// import React from "react";
-// import "../styles/navbar.css"; // Crearemos estilos para la barra de navegación en este archivo
-// import mano from "../assets/mano.png";
-// import soga from "../assets/soga-nada-sinF.png";
-// import madera from "../assets/boton-madera.png";
-
-// const Navbar = () => {
-//   return (
-//     <div className="navbar-container">
-//       {/* Mano */}
-//       <img
-//         src={mano}
-//         alt="Mano"
-//         style={{
-//           position: "absolute",
-//           top: "50%",
-//           left: "50%",
-//           transform: "translate(-50%, -50%)",
-//         }}
-//       />
-
-//       {/* Soga superior */}
-//       <img
-//         src={soga}
-//         alt="Soga"
-//         style={{
-//           position: "absolute",
-//           top: "0",
-//           left: "50%",
-//           transform: "translateX(-50%)",
-//         }}
-//       />
-
-//       {/* Soga inferior */}
-//       <img
-//         src={soga}
-//         alt="Soga"
-//         style={{
-//           position: "absolute",
-//           bottom: "0",
-//           left: "50%",
-//           transform: "translateX(-50%)",
-//         }}
-//       />
-
-//       {/* Maderas */}
-//       <img
-//         src={madera}
-//         alt="Madera"
-//         style={{ position: "absolute", top: "0", left: "10%" }}
-//       />
-//       <img
-//         src={madera}
-//         alt="Madera"
-//         style={{ position: "absolute", top: "0", left: "30%" }}
-//       />
-//       <img
-//         src={madera}
-//         alt="Madera"
-//         style={{ position: "absolute", top: "0", left: "50%" }}
-//       />
-//       <img
-//         src={madera}
-//         alt="Madera"
-//         style={{ position: "absolute", top: "0", left: "70%" }}
-//       />
-//       <img
-//         src={madera}
-//         alt="Madera"
-//         style={{ position: "absolute", top: "0", left: "90%" }}
-//       />
-//     </div>
-//   );
-// };
-
-// export default Navbar;
