ae033475b956b0bc350ef5e77ee2e4f85f687ad5
Gonzalo <molinagomezgonzalo@gmail.com>
2023-08-06 00:12:08 -0300

    feat: text added to buttons

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import "./App.css";
 import botonMadera from "./assets/boton-madera.png";
 import soga from "./assets/soga-nada-sinF.png";
 import Navbar from "./components/Navbar";
+import "./assets/fonts/sifonn/nuevo-sifonn/sifonn/stylesheet.css";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <h1 className="nombre">
+        <span>Gonzalo</span>
+        <span>Molina</span>
+        <span className="profession">Full Stack Developer</span>
+      </h1>
     </div>
   );
 }
---
+++ src/commons/button.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "../styles/button.css";
+
+import madera from "../assets/boton-madera.png";
+
+const Button = ({ length, title, lengthText }) => {
+  return (
+    <div>
+      <img
+        src={madera}
+        alt="Madera"
+        className="madera"
+        style={{ left: length }}
+      />
+      <p
+        className="texto"
+        style={{
+          left: lengthText,
+        }}
+      >
+        {title}
+      </p>
+    </div>
+  );
+};
+
+export default Button;
